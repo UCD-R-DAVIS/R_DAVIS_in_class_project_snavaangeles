0aee3ee6b1281a17b413d6f180a28b3a3bfee279
Stephanie Nava-Angeles <Stephanie@campus-005-242.ucdavis.edu>
2024-10-03 13:56:51 -0700

    elephants

---
+++ Week 2 Intro to R.R
@@ -0,0 +1,28 @@
+3+4
+2*
+2+5
+4+(8*3)^2
+#learning order of operations
+4+(8*3)^2
+exp (1)
+?log
+log(4,2)
+log(2,4)
+log (x=2, base =4)
+x<-1
+x<-2
+x<-1
+log(base =4, x =2)
+rm(x)
+mynumber<-5
+mynumber ==5
+numSamples <-50
+othernumber <-3
+num_samples <-3
+num_Samples <-2
+rm(list=(ls()))
+log("A_Word")
+log_of_word <- log("a_word")
+elephant1_kg <-3492
+rm(elephant2_lb)
+elephant1_lb <-7757
